b2813d61f3f10e738f558dfbffcbe93547da0fa1
i4igo <petruxaxa@gmail.com>
2017-01-17 00:34:56 +0200

    add constants

--- app/src/main/java/ua/i4igo/tracker/Constants.java
+++ app/src/main/java/ua/i4igo/tracker/Constants.java
@@ -9,10 +9,15 @@ public interface Constants {
     String KEY_LOGIN = "login";
     String KEY_PASSWORD = "password";
 
-    String KEY_NAME = "name";
-    String KEY_LAST_NAME = "lName";
-    String KEY_PHOTO = "photo";
-    String KEY_PHONE = "phone";
-    String KEY_EMAIL = "eMail";
-    String KEY_CITY = "city";
+
+    String KEY_USER_PHOTO = "photo";
+    String KEY_USER_PHONE = "phone";
+    String KEY_USER_PASSWORD = "password";
+    String KEY_USER_CONFIRM_PASSWORD = "confPassword";
+    String KEY_USER_NAME = "name";
+    String KEY_USER_LAST_NAME = "lName";
+    String KEY_USER_EMAIL = "eMail";
+    String KEY_USER_CITY = "city";
+
+    String FILE_SAVE = "pref_user";
 }
