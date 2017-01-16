3c4b644e453e6c93bca851d34a16b7a5d12a5744
i4igo <petruxaxa@gmail.com>
2017-01-17 00:34:28 +0200

    modify

--- app/src/main/java/ua/i4igo/tracker/AccountUser.java
+++ app/src/main/java/ua/i4igo/tracker/AccountUser.java
@@ -10,17 +10,20 @@ public class AccountUser {
 
     private String name;
     private String lName;
-    private String photo;
+    private String password;
+    private String confirmPassword;
     private String phone;
     private String eMail;
     private String city;
+
     private SharedPreferences preferences;
 
-    public AccountUser(String name, String lName, String photo, String phone, String eMail, String city) {
+    public AccountUser(String phone, String password, String confirmPassword, String name, String lName, String eMail, String city) {
+        this.phone = phone;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
         this.name = name;
         this.lName = lName;
-        this.photo = photo;
-        this.phone = phone;
         this.eMail = eMail;
         this.city = city;
     }
@@ -33,8 +36,12 @@ public class AccountUser {
         return lName;
     }
 
-    public String getPhoto() {
-        return photo;
+    public String getPassword() {
+        return password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
     }
 
     public String getPhone() {
@@ -51,18 +58,42 @@ public class AccountUser {
 
     /**
      * сохраняем в preferences данные
+     *
+         preferences = getSharedPreferences(Constants.FILE_SAVE, MODE_PRIVATE);
+         SharedPreferences.Editor editor = preferences.edit()
+             .putString(Constants.KEY_USER_PHONE, etRPhone.getText().toString())
+             .putString(Constants.KEY_USER_PASSWORD, etRPassword.getText().toString())
+             .putString(Constants.KEY_USER_CONFIRM_PASSWORD, etRConfirmPass.getText().toString())
+             .putString(Constants.KEY_USER_NAME, etRName.getText().toString())
+             .putString(Constants.KEY_USER_LAST_NAME, etRLName.getText().toString())
+             .putString(Constants.KEY_USER_EMAIL, etREMail.getText().toString())
+             .putString(Constants.KEY_USER_CITY, etRCity.getText().toString())
+             .commit()
+
      */
+    public void saveInPreferencesUser(String string) {
+
+    }
+
     public void saveName(String name) {
         this.name = name;
+        saveInPreferencesUser(name);
     }
+
+
 
     public void savelName(String lName) {
         this.lName = lName;
 
     }
 
-    public void savePhoto(String photo) {
-        this.photo = photo;
+    public void savePassword(String password) {
+        this.password = password;
+
+    }
+
+    public void saveConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
 
     }
 
